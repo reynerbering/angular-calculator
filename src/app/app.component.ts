384a5ce5a5e543461157c712b1fa7b0d5574a068
Neru <reynerpaulbering@gmail.com>
2021-10-04 14:07:51 +0800

    Added Angular Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
-}
+
+  usrInput1  = 0;
+  usrInput2  = 0;
+  result = 0;
+
+
+  num1(value : any){
+    this.usrInput1 = parseInt(value);
+    console.log(this.usrInput1);
+  }
+  num2(value : any){
+    this.usrInput2 = parseInt(value);
+    console.log(this.usrInput2);
+    
+    
+  }
+  onClickAdd(){
+    this.result = (this.usrInput1 + this.usrInput2);
+    let resultOutput = document.getElementById("result") as HTMLInputElement;
+    resultOutput.valueAsNumber = this.result;
+  }
+  onClickSubtract(){
+    this.result = (this.usrInput1 - this.usrInput2);
+    let resultOutput = document.getElementById("result") as HTMLInputElement;
+    resultOutput.valueAsNumber = this.result;
+  }
+  onClickMultiply(){
+    this.result = (this.usrInput1 * this.usrInput2);
+    let resultOutput = document.getElementById("result") as HTMLInputElement;
+    resultOutput.valueAsNumber = this.result;
+  }
+  onClickDivide(){
+    this.result = (this.usrInput1 / this.usrInput2);
+    let resultOutput = document.getElementById("result") as HTMLInputElement;
+    resultOutput.valueAsNumber = this.result;
+  }
+  }
